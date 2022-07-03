e8b96731d1e75de67e929a825fd126ad6bd82f1a
kunal <kunalbankoti9@gmail.com>
2022-07-03 15:18:49 +0530

    add About & Interests components

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Info from './Info';
+import About from './About';
+import Interests from './Interests';
 
 function App() {
     return (
         <div className='app'>
             <Info />
+            <About />
+            <Interests />
         </div>
     );
 }
